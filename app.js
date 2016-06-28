dd3a34fc1969f8d8b4adc673d8b4b6ddf412d534
rodolfocruzbsb <rodolfocruz.ti@gmail.com>
2016-06-28 20:41:21 -0300

    atualizando arquitetura

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('./config/express')();
 
 var http = require('http').Server(app);
 
-//Caso não tenha porta definida na variável de ambiente(definida pelo heroku), usa a 3000
+//Caso não tenha porta definida na variável de ambiente(definida pelo heroku), usa a 8080
 var port = process.env.PORT || 8080;
 
 http.listen(port, function(){
